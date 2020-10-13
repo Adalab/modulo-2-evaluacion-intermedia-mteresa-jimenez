b8b053012a14a87ac14f2ee411cae63369537215
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-13 11:00:03 +0200

    Add hint box message in js

--- main.js
+++ main.js
@@ -3,23 +3,26 @@
 const number = document.querySelector(".js-number");
 const button = document.querySelector(".js-button");
 const hint = document.querySelector(".js-hint");
-let hintBox = hint.innerHTML;
 
-function getRandomNumber(max) {
-  return Math.ceil(Math.random() * max);
-}
-console.log(getRandomNumber);
+let random = Math.ceil(Math.random() * 100);
+
+console.log(random);
 
 function handleButtonClick() {
-  if (number.value === getRandomNumber) {
-    hintBox = "Has ganado campeona!!!";
+  let numberValue = parseInt(number.value);
+  console.log(numberValue);
+  if (numberValue === random) {
+    hint.value = "Has ganado campeona!!!";
     console.log("Has ganado campeona!!!");
-  } else if (number.value < getRandomNumber) {
-    hintBox = "nonono";
+  } else if (numberValue < random && numberValue >= 1 && numberValue <= 100) {
+    hint.value = "Demasiado bajo.";
     console.log("Demasiado bajo.");
-  } else if (number.value > getRandomNumber) {
-    hintBox = "nonono";
+  } else if (numberValue > random && numberValue >= 1 && numberValue <= 100) {
+    hint.value = "Demasiado alto.";
     console.log("Demasiado alto.");
+  } else if (numberValue <= 0 || numberValue > 100) {
+    hint.value = "El número debe estar entre 1 y 100.";
+    console.log("El número debe estar entre 1 y 100.");
   }
 }
 

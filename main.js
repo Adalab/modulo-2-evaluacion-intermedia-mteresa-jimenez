ead8d0dcd85ffc7d6e16b98c0e9b66093c54ee41
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-26 13:58:43 +0100

    Fix issue innerHTML

--- main.js
+++ main.js
@@ -16,19 +16,26 @@ console.log(random);
 function handleButtonClick() {
   let numberValue = parseInt(number.value);
   console.log(numberValue);
+  let html = "";
+
   if (numberValue === random) {
-    hint.innerHTML = "Has ganado campeona!!!";
+    html = "Has ganado campeona!!!";
+    // hint.innerHTML = "Has ganado campeona!!!";
     console.log("Has ganado campeona!!!");
   } else if (numberValue < random && numberValue >= 1 && numberValue <= 100) {
-    hint.innerHTML = "Demasiado bajo.";
+    html = "Demasiado bajo.";
+    // hint.innerHTML = "Demasiado bajo.";
     console.log("Demasiado bajo.");
   } else if (numberValue > random && numberValue >= 1 && numberValue <= 100) {
-    hint.innerHTML = "Demasiado alto.";
+    html = "Demasiado alto.";
+    // hint.innerHTML = "Demasiado alto.";
     console.log("Demasiado alto.");
   } else if (numberValue <= 0 || numberValue > 100) {
-    hint.innerHTML = "El número debe estar entre 1 y 100.";
+    html = "El número debe estar entre 1 y 100.";
+    // hint.innerHTML = "El número debe estar entre 1 y 100.";
     console.log("El número debe estar entre 1 y 100.");
   }
+  hint.innerHTML = html;
 
   attempts.innerHTML = `Número de intentos: ${addClick()}`;
 }
